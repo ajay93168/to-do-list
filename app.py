8c4c61c7014fd25a0d85b1c431691ebae55d7407
Ajay <you@example.com>
2025-02-01 15:57:18 -0600

    req added

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def delete_task(id):
         return "There was an issue deleting the task."
 
 # Run the app
-# Run the app
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database tables if they don't exist
